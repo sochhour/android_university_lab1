f6df50f4481a185a34ed77aae453ac890cb61db6
sochhour <sophie.chor.kaplan@emory.edu>
2022-12-09 21:26:14 -0500

    update api key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "Xt1er17WyO3clgwU5Mh449FPqIQg1Bfp";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
